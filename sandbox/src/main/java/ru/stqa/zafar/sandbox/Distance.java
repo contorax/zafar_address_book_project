2b089dc2c84f2eeabc7431b8d5ec2e57f71fe1c6
Zafar Tagiyev <contorax@yandex.com>
2018-03-19 17:15:48 -0500

    вычисление при помощи метода в классе Point и вызывов метода надо в запускаемом классе Distance

--- sandbox/src/main/java/ru/stqa/zafar/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/zafar/sandbox/Distance.java
@@ -5,22 +5,19 @@ public class Distance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(0,3);
-    Point p2 = new Point(8,9);
+    Point p1 = new Point(0.0 ,3.0);
+    Point p2 = new Point(8.0 ,9.0);
 
 
     System.out.println( "Точка p1: x = " + p1.x + ", y = " + p1.y );
     System.out.println( "Точка p2: x = " + p2.x + ", y = " + p2.y );
-    System.out.println( "Расстояние между точками p1 и p2 = " + distance( p1, p2 ) );
+    System.out.println( "Расстояние между точками p1 и p2 = " + p1.distance(p2) ) ;
 
   }
 
-  public static double distance(Point p1, Point p2) {
-    double distance = Math.sqrt( (Math.pow( (p2.x - p1.x), 2 ) + Math.pow( (p2.y - p1.y), 2 )) );
-    return distance;
   }
 
 
-}
+
 
 
--- sandbox/src/main/java/ru/stqa/zafar/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/zafar/sandbox/Point.java
@@ -4,11 +4,26 @@ public class Point {
   double x;
   double y;
 
-  Point(double x, double y) {
-    this.x = x;
-    this.y = y;
 
+
+
+
+
+  public Point(double x, double y) {
+  this.x = x;
+  this.y = y;
+  }
+
+
+  public double distance(Point p2) {
+
+
+    double distance = Math.sqrt( (Math.pow( (p2.x - this.x), 2 ) + Math.pow( (p2.y - this.y), 2 )) );
+
+    return distance ;
   }
 
 
 }
+
+
