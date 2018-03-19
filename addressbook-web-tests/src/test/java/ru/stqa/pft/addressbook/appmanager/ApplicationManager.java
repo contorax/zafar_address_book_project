fbd7be6812df9d8b461574d963588a16a937244f
Zafar Tagiyev <contorax@yandex.com>
2018-03-19 02:29:48 -0500

    для вспомогательных классов Создан общий базовый класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -11,39 +10,32 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
   FirefoxDriver wd;
   private SessionHelper sessionHelper;
-  private NavigationHelper navigationHelper ;
-  private GroupHelper groupHelper ;
-
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-      try {
-          wd.switchTo().alert();
-          return true;
-      } catch (NoAlertPresentException e) {
-          return false;
-      }
-  }
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
 
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/addressbook/");
+    wd = new FirefoxDriver( new FirefoxOptions().setLegacy( true ).setBinary( "C:/Program Files/Mozilla Firefox/firefox.exe" ) );
+    wd.manage().timeouts().implicitlyWait( 60, TimeUnit.SECONDS );
+    wd.get( "http://localhost/addressbook/addressbook/" );
     groupHelper = new GroupHelper( wd );
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    navigationHelper = new NavigationHelper( wd );
+    sessionHelper = new SessionHelper( wd );
+    sessionHelper.login( "admin", "secret" );
   }
 
   private void login(String username, String password) {
-      wd.findElement(By.name("user")).click();
-      wd.findElement(By.name("user")).clear();
-      wd.findElement(By.name("user")).sendKeys(username);
-      wd.findElement(By.name("pass")).click();
-      wd.findElement(By.name("pass")).clear();
-      wd.findElement(By.name("pass")).sendKeys(password);
-      wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    wd.findElement( By.name( "user" ) ).click();
+    wd.findElement( By.name( "user" ) ).clear();
+    wd.findElement( By.name( "user" ) ).sendKeys( username );
+    wd.findElement( By.name( "pass" ) ).click();
+    wd.findElement( By.name( "pass" ) ).clear();
+    wd.findElement( By.name( "pass" ) ).sendKeys( password );
+    wd.findElement( By.xpath( "//form[@id='LoginForm']/input[3]" ) ).click();
   }
 
-  public void stop() {wd.quit(); }
+  public void stop() {
+    wd.quit();
+  }
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
