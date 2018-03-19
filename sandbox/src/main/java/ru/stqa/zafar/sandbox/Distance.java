bc4275ac35cd18f50ceadde1367f8c0c68c8505f
Zafar Tagiyev <contorax@yandex.com>
2018-03-19 00:13:02 -0500

    в классе Point создан конструктор
    This reverts commit c8c49417bf12ac631f7bf03155595b3c776f6e42.

--- sandbox/src/main/java/ru/stqa/zafar/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/zafar/sandbox/Distance.java
@@ -4,10 +4,11 @@ public class Distance {
 
 
   public static void main(String[] args) {
+
     Point p1 = new Point(0,3);
     Point p2 = new Point(8,9);
-   
 
+   
     System.out.println( "Точка p1: x = " + p1.x + ", y = " + p1.y );
     System.out.println( "Точка p2: x = " + p2.x + ", y = " + p2.y );
     System.out.println( "Расстояние между точками p1 и p2 = " + distance( p1, p2 ) );
--- sandbox/src/main/java/ru/stqa/zafar/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/zafar/sandbox/Point.java
@@ -1,13 +1,14 @@
 package ru.stqa.zafar.sandbox;
 
 public class Point {
-   double x;
-   double y;
+  double x;
+  double y;
 
-  Point (double x, double y){
+  Point(double x, double y) {
     this.x = x;
     this.y = y;
 
   }
+
 
 }
