d4f6b4a52b15a23d132cd71cc0d9e211b871c623
Zafar Tagiyev <contorax@yandex.com>
2018-03-22 16:07:23 -0500

    Failed Test (несоответсвие целое число и число с плавающей точкой)

--- sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
@@ -10,6 +10,6 @@ public class PointTests {
     Distance d = new Distance();
     Point p1 = new Point( 0.0, 3.0 );
     Point p2 = new Point( 8.0, 9.0 );
-    Assert.assertEquals( p1.distance( p2 ), 10.0 );
+    Assert.assertEquals( p1.distance( p2 ), 10 );
   }
 }
