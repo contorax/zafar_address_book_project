4e98b2d87e0ca5e5a8ff12296a201f19d7592685
Zafar Tagiyev <contorax@yandex.com>
2018-03-22 16:30:25 -0500

    первый положительный тест

--- sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
@@ -1,15 +1,13 @@
 package ru.stqa.zafar.sandbox;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+        import org.testng.annotations.Test;
 
 public class PointTests {
   @Test
-  public void TestDistance() {
+  public void TestDistance () {
 
     Distance d = new Distance();
     Point p1 = new Point( 0.0, 3.0 );
     Point p2 = new Point( 8.0, 9.0 );
-    Assert.assertEquals( p1.distance( p2 ), 10 );
-  }
+    assert p1.distance( p2 ) == 10;  }
 }
