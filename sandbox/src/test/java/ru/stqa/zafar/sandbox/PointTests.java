704ded61f8d36b87fc18869297b0b03f7bfc1061
Zafar Tagiyev <contorax@yandex.com>
2018-03-22 16:32:33 -0500

    упавший тест - ошибка в результате

--- sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
@@ -9,5 +9,5 @@ public class PointTests {
     Distance d = new Distance();
     Point p1 = new Point( 0.0, 3.0 );
     Point p2 = new Point( 8.0, 9.0 );
-    assert p1.distance( p2 ) == 10;  }
+    assert p1.distance( p2 ) == 9;  }
 }
