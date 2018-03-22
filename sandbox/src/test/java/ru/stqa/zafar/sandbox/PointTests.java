9ea7f943f577f2fe93c7e5dadb293e830f83036c
Zafar Tagiyev <contorax@yandex.com>
2018-03-22 16:37:33 -0500

    Failed Test с функцией assert ( expected and actual results)

--- sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
@@ -1,13 +1,17 @@
 package ru.stqa.zafar.sandbox;
 
-        import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class PointTests {
   @Test
-  public void TestDistance () {
+  public void TestDistance() {
 
     Distance d = new Distance();
     Point p1 = new Point( 0.0, 3.0 );
     Point p2 = new Point( 8.0, 9.0 );
-    assert p1.distance( p2 ) == 9;  }
+    Assert.assertEquals( p1.distance( p2 ), 9 );
+  }
+
 }
+    
