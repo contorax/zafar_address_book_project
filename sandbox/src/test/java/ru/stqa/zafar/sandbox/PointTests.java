e05f1e993f423fff40f919cde6a747594eeb54cd
Zafar Tagiyev <contorax@yandex.com>
2018-03-22 16:05:18 -0500

    положительный тест - c функцией assertEquals  результат- число с плавающей точкой

--- sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/zafar/sandbox/PointTests.java
@@ -4,13 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTests {
-@Test
-  public void TestDistance () {
+  @Test
+  public void TestDistance() {
 
-    Distance d = new Distance ();
-    d.p1 = 8;
-    d.p2 = 2;
-  Assert.assertEquals(d.distance(), 6.0);
+    Distance d = new Distance();
+    Point p1 = new Point( 0.0, 3.0 );
+    Point p2 = new Point( 8.0, 9.0 );
+    Assert.assertEquals( p1.distance( p2 ), 10.0 );
   }
 }
-
