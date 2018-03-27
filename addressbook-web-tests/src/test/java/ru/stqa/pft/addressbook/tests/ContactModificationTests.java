22fd0ce4815ba9931addb8e08697f1fcbec4ef85
Zafar Tagiyev <contorax@yandex.com>
2018-03-27 00:42:58 -0500

    Перенос тестов ContactCreation, ContactDeletion, TestBase в пакет Tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.TestBaseContacts;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactModificationTests extends TestBaseContacts {
