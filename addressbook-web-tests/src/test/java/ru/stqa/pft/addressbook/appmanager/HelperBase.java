c8c2f347bcdd2e7c750186a0e8a12e9d345df45f
Zafar Tagiyev <contorax@yandex.com>
2018-03-28 00:02:57 -0500

    keep existing value in fields

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -18,11 +18,13 @@ public class HelperBase {
   protected void type(By locator, String text) {
     click( locator );
     if (text != null) {
-      wd.findElement( locator ).clear();
-      wd.findElement( locator ).sendKeys( text );
+      String existingText = wd.findElement( locator ).getAttribute( "value" );
+      if (!text.equals( existingText )) {
+        wd.findElement( locator ).clear();
+        wd.findElement( locator ).sendKeys( text );
+      }
     }
   }
-
   public boolean isAlertPresent() {
     try {
       wd.switchTo().alert();
@@ -40,8 +42,11 @@ public class HelperBase {
   protected void typecontact(By locator, String text) {
     clickcontact( locator );
     if (text != null) {
-      wd.findElement( locator ).clear();
-      wd.findElement( locator ).sendKeys( text );
+      String existingText = wd.findElement( locator ).getAttribute( "value" );
+      if (!text.equals( existingText )) {
+        wd.findElement( locator ).clear();
+        wd.findElement( locator ).sendKeys( text );
+      }
     }
   }
 }
