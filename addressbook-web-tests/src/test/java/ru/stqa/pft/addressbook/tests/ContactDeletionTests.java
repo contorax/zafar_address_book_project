13199476b979b13cec76c6e512239a78b11490a9
Zafar Tagiyev <contorax@yandex.com>
2018-04-03 15:00:43 -0500

    Count of Contact after Deletion and Modification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -15,15 +16,17 @@ public class ContactDeletionTests extends TestBase {
       app.getGroupHelper().createGroup( new GroupData( "test1", null, null ) );
     }
     app.getNavigationHelper().gotoHomePage();
+
     if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact( new ContactData( "Maria", "Davis", null, null, "test1" ), true );
     }
-
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
     app.getContactHelper().closeAlertWindow();
-
-
+    app.getNavigationHelper().gotoHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before - 1  );
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -14,15 +15,18 @@ public class ContactModificationTests extends TestBase {
       app.getGroupHelper().createGroup( new GroupData( "test1", null, null ) );
     }
     app.getNavigationHelper().gotoHomePage();
+
     if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact( new ContactData( "Maria", "Davis", null, null, "test1" ), true );
     }
-
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm( new ContactData( "Adam", "Smith", "7654321", "justtest@mail.ru", null ), false );
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before  );
   }
 
 
