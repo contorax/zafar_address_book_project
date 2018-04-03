0f26a66e3e1318057d665fdf0633d9c0719dd292
Zafar Tagiyev <contorax@yandex.com>
2018-04-02 23:34:11 -0500

    while loops - isPrime

--- sandbox/src/main/java/ru/stqa/zafar/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/zafar/sandbox/Primes.java
@@ -11,4 +11,30 @@ public class Primes {
     return true;
 
   }
+
+  public static boolean isPrimeWhile (int n) {
+    int i = 2;
+    while (i < n){
+      if (n % i == 0) {
+        return false;
+    }
+    i++;
+    }
+    return true;
+
+  }
+
+
+// в случае с while можно избавиться от конструкции if  и эту проверку включить в условие окончания цикла
+// while (i < n && n % i != 0) то есть проверка продолжается до тех пор пока i < n  и остаток от деления i на n не равно 0 то есть  n на i
+ // не делится, тогда продолжаем искать делитель дальше
+
+  public static boolean isPrimeWhile2 (int n) {
+    int i = 2;
+    while (i < n && n % i != 0){
+           i++;
+    }
+    return i == n;
+
+  }
 }
