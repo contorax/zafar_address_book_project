da8ac60282c24306fea8b0e92c6c29ccf15ea667
Zafar Tagiyev <contorax@yandex.com>
2018-05-05 18:54:19 -0500

    4.9 search for the maximum identifier with lambda expressions (GroupCreation)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,34 +8,32 @@ import java.util.HashSet;
 import java.util.List;
 
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+  @Test
+  public void testGroupCreation() {
 
-        app.getNavigationHelper().gotoGroupPage();
-       // тут сравниваем размеры списков, которые получены с при помощи getGroupList (а следующем модуле будет сравнение списков целиком)
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-       GroupData group = new GroupData("test1", null, null);
-        app.getGroupHelper().createGroup( group );
-        List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size()+1 );
+    app.getNavigationHelper().gotoGroupPage();
+    // тут сравниваем размеры списков, которые получены с при помощи getGroupList (а следующем модуле будет сравнение списков целиком)
+    List <GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData( "test2", null, null );
+    app.getGroupHelper().createGroup( group );
+    List <GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals( after.size(), before.size() + 1 );
 
 
+    // Лекция 4.9. Список можно превратить в поток.  у этого поток есть метод мах для вычисл макс элемента
+    // переменую нужно инитилизировать - присвоить ей значение
+// Comparator это интерфейс который не имеет реализации, он только декларирует-объявляет какие методы должжны быть, но реализацией этих методов не содержит
 
-        // 4.8 устраиваем цикл по всем элементам
-      int max =0;
-      for (GroupData g: after){
-        if (g.getId()> max){
-          max = g.getId();
-        }
-      }
-      group.setId(max);
-      before.add (group);
-        // (4.8) среди всех элементов входящих в новый списко нужно найти тот который имеет максимальный идентификатор
+    // 4.9. локальную переменную удаляем  int max1 = after.stream().max( (o1, o2) -> Integer.compare(o1.getId(), o2.getId()  ) ).get().getId();
 
-      // (4.8) чтобы сравнить два списка before & after без учета порядка, надо 2 списка преоброзовать в множество (HashSet<Object>) и сравнивать их в таком виде
-      Assert.assertEquals( new HashSet<Object>( before ), new HashSet <Object>( after ) );
-    }
+    group.setId( after.stream().max( (o1, o2) -> Integer.compare( o1.getId(), o2.getId() ) ).get().getId() );
+    before.add( group );
+    // (4.8) среди всех элементов входящих в новый списко нужно найти тот который имеет максимальный идентификатор
+
+    // (4.8) чтобы сравнить два списка before & after без учета порядка, надо 2 списка преоброзовать в множество (HashSet<Object>) и сравнивать их в таком виде
+    Assert.assertEquals( new HashSet <Object>( before ), new HashSet <Object>( after ) );
+  }
 
 }
